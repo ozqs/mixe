981679b2b5b5a3019469cffdc3c6672ee553d2a9
ozqs <gh-ozqs@hotmail.com>
2024-07-19 14:09:17 +0000

    first

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,46 @@
 use std::cmp::Ordering;
-type word = i32;
 
-#[derive(Clone, Copy)]
+// type MIXWord = i32;
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct MIXWord(u32);
+
+impl From<u32> for MIXWord {
+    fn from(a: u32) -> Self {
+        MIXWord(a)
+    }
+}
+
+impl From<(u32, u32, u32, u32, u32, u32)> for MIXWord {
+    fn from(a: (u32, u32, u32, u32, u32, u32)) -> Self {
+        MIXWord((a.0 << 31) + (a.1 << 20) + (a.2 << 15) + (a.3 << 10) + (a.4 << 5) + (a.5))
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 struct Unit {}
 
 impl Unit {
-    fn new() -> Self {Unit{} }
+    fn new() -> Self {
+        Unit {}
+    }
 }
 
 pub struct MIXComputer {
-    pub register: [word; 9],
+    pub register: [MIXWord; 9],
     pub overflow: bool,
     pub comp: Ordering, // -1 0 1
     pub units: [Unit; 16],
-    pub memory: [word; 4000],
+    pub memory: [MIXWord; 4000],
 }
 
 impl MIXComputer {
     fn new() -> Self {
         MIXComputer {
-            register: [0; 9],
+            register: [0u32.into(); 9],
             overflow: false,
             comp: Ordering::Less,
             units: [Unit::new(); 16],
-            memory: [0; 4000],
+            memory: [0u32.into(); 4000],
         }
     }
 }
@@ -40,13 +57,17 @@ impl MIXCPU {
             computer,
         }
     }
-    
-    pub fn excute(ins: i32) {
+
+    pub fn excute(&mut self, ins: i32) {
         todo!()
     }
 
-    pub fn run(command: &str) {
-        todo!()
+    // to solve a command str mentioned in the Book.
+    pub fn run(&mut self, command: &str) {
+        let mut s = command.split(' ');
+        if let(op) = s.next().unwrap() {
+            
+        }
     }
 }
 
@@ -56,19 +77,20 @@ mod tests {
     #[test]
     fn test_load() {
         let mut computer = MIXComputer::new();
-        computer.memory[2000] = -((80 << 15) + (3 << 10) + (5 << 5) + 4);
-        let computer = MIXCPU::from(computer);
+        // computer.memory[2000] = ((1 << 31) + (80 << 15) + (3 << 10) + (5 << 5) + 4).into();
+        computer.memory[2000] = (1, 0, 80, 3, 5, 4).into();
+        let mut computer = MIXCPU::from(computer);
         computer.run("LDA 2000");
-        assert_eq!(computer.computer.register[0], -((80 << 15) + (3 << 10) + (5 << 5) + 4));
+        assert_eq!(computer.computer.register[0], (1, 0, 80, 3, 5, 4).into());
         computer.run("LDA 2000(1:5)");
-        assert_eq!(computer.computer.register[0], ((80 << 15) + (3 << 10) + (5 << 5) + 4));
+        assert_eq!(computer.computer.register[0], (0, 0, 80, 3, 5, 4).into());
         computer.run("LDA 2000(3:5)");
-        assert_eq!(computer.computer.register[0], ((3 << 10) + (5 << 5) + 4));
+        assert_eq!(computer.computer.register[0], (0, 0, 0, 3, 5, 4).into());
         computer.run("LDA 2000(0:3)");
-        assert_eq!(computer.computer.register[0], -((80 << 5) + 3));
+        assert_eq!(computer.computer.register[0], (1, 0, 0, 0, 80, 3).into());
         computer.run("LDA 2000(4:4)");
-        assert_eq!(computer.computer.register[0], 5);
+        assert_eq!(computer.computer.register[0], 5.into());
         computer.run("LDA 2000(0:0)");
-        assert_eq!(computer.computer.register[0], -0);
+        assert_eq!(computer.computer.register[0], (1, 0, 0, 0, 0, 0).into());
     }
 }
