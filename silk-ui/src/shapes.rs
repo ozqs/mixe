83906bb06b0af18419c0fe5b5801dee781235bbe
Your Name <you@example.com>
2024-09-22 12:32:24 +0800

    Capsule

--- silk-ui/src/shapes.rs
+++ silk-ui/src/shapes.rs
@@ -1,1 +1,5 @@
 pub mod capsule;
+
+trait Drawable {
+    fn draw(&self);
+}
--- silk-ui/src/shapes/capsule.rs
+++ silk-ui/src/shapes/capsule.rs
@@ -1,19 +1,24 @@
 use macroquad::prelude::*;
+use crate::draw::draw_capsule;
+use crate::fluent::Interpolatable;
+use super::Drawable;
 
 pub struct Capsule {
     left: f32,
     top: f32,
     width: f32,
     height: f32,
+    color: Color,
 }
 
 impl Capsule {
-    fn new(left: f32, top: f32, width: f32, height: f32) -> Self {
+    fn new(left: f32, top: f32, width: f32, height: f32, color: Color) -> Self {
         Self {
             left,
             top,
             width,
             height,
+            color,
         }
     }
 
@@ -23,13 +28,32 @@ impl Capsule {
     }
 }
 
-impl From<Rect> for Capsule {
-    fn from(value: Rect) -> Self {
+impl Drawable for Capsule {
+    fn draw(&self) {
+        draw_capsule(self.left, self.top, self.width, self.height, self.color);
+    }
+}
+
+impl From<(Rect, Color)> for Capsule {
+    fn from((value, color): (Rect, Color)) -> Self {
         Self {
             left: value.x,
             top: value.y,
             width: value.w,
             height: value.h,
+            color,
+        }
+    }
+}
+
+impl Interpolatable for Capsule {
+    fn interpolate(&self, other: &Self, progress: f32) -> Self {
+        Self {
+            left: self.left + (other.left - self.left) * progress,
+            top: self.top + (other.top - self.top) * progress,
+            width: self.width + (other.width - self.width) * progress,
+            height: self.height + (other.height - self.height) * progress,
+            color: (self.color.to_vec() + (other.color.to_vec() - self.color.to_vec()) * progress).into(),
         }
     }
 }
